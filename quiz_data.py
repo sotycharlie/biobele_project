49032376cf6d4881328010925ded710f2502dc67
Charles Sotonye <87846328+sotycharlie@users.noreply.github.com>
2024-01-15 19:40:18 +0100

    v1
    v1

---
+++ quiz_data.py
@@ -0,0 +1,24 @@
+quiz_data = [
+    {
+        "question": "What is the capital of Nigeria?",
+        "choices": ["Abuja", "Lagos", "Port harcourt", "Calabar"],
+        "answer": "Abuja"
+    },
+    {
+        "question": "What is the largest country in Africa?",
+        "choices": ["Sudan", "Congo", "Nigeria", "Algeria"],
+        "answer": "Algeria"
+    },
+    {
+        "question": "What is the most reactive element?",
+        "choices": ["Go", "Au", "F", "O"],
+        "answer": "F"
+    },
+    {
+        "question": "Which country's motto is known as the 'Unity In Diversity'?",
+        "choices": ["Iceland", "India", "Indonesia", "Iraq"],
+        "answer": "Indonesia"
+    }
+    # Add more questions 
+  
+]
